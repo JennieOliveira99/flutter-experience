4260d30e7480dfe3da238a3a614a5040034bbaaf
Jennyfer Alvim <jennyferoliveira7315@gmail.com>
2025-08-16 18:14:28 -0300

    adicionando configuracoes do riverpod, Rest client, otimizando o buildrunner e adicionando todas as integracoes com a api e google cloud

---
+++ projeto/android/app/src/main/kotlin/com/githubJennieOliveira99/cinebox/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.githubJennieOliveira99.cinebox
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ projeto/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:cinebox/cinebox_main_app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp( ProviderScope(
+    child:CineboxMainApp(),
+  ));
+}
